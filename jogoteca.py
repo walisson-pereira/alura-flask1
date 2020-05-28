447bae6f7b7a689ffc6a439a7bbe0bf7638784bc
Walisson Pereira <walisson_pereira@uvanet.br>
2020-05-28 12:55:01 -0300

    curso 2 - aula 02

--- jogoteca.py
+++ jogoteca.py
@@ -29,6 +29,12 @@ def novo():
         return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+    jogo = jogo_dao.busca_por_id(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
 
 @app.route('/criar', methods=['POST', ])
 def criar():
@@ -39,6 +45,20 @@ def criar():
     jogo_dao.salvar(jogo)
     return redirect(url_for('index'))
 
+@app.route('/atualizar', methods=['POST', ])
+def atualizar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console, id=request.form['id'])
+    jogo_dao.salvar(jogo)
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    jogo_dao.deletar(id)
+    flash('O jogo foi removido com sucesso')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
@@ -65,6 +85,7 @@ def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
     return redirect(url_for('index'))
+
 
 
 app.run(debug=True)
